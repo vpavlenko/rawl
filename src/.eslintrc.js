c97fa737c576d6fa522343ee0739b420a94abf88
ryohey <info@codingcafe.jp>
2016-05-23 09:48:08 +0900

    add eslint

---
+++ src/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+}

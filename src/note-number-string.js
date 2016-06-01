5d3229865a2b5128a2957216e60d7d3862fdfe12
ryohey <info@codingcafe.jp>
2016-06-02 01:15:28 +0900

    fix noteNumberString to octave start from -1

--- src/note-number-string.js
+++ src/note-number-string.js
@@ -2,7 +2,7 @@ const NOTE_NAMES = [
   "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"
 ]
 function noteNumberString(noteNumber) {
-  const oct = Math.floor(noteNumber / 12)
+  const oct = Math.floor(noteNumber / 12) - 1
   const key = noteNumber % 12
   return `${NOTE_NAMES[key]} ${oct} (${noteNumber})`
 }

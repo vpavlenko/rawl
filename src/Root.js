65eb580ccc73db2a7b6bf97cd183ed33c82f7017
ryohey <info@codingcafe.jp>
2016-10-11 00:01:43 +0900

    add withSong

--- src/Root.js
+++ src/Root.js
@@ -1,19 +1,41 @@
 import React, { Component } from "react"
 import RootView from "./components/RootView"
 
-export default class Root extends Component {
-  constructor(props) {
-    super(props)
+function withSong(WrappedComponent) {
+  return class extends Component {
+    constructor(props) {
+      super(props)
 
-    this.initKeyboardShortcut()
+      this.state = {
+        song: props.app.song
+      }
 
-    this.state = {
-      song: props.app.song
+      props.app.on("change-song", song => {
+        this.setSong(song)
+      })
     }
 
-    props.app.on("change-song", song => {
-      this.setSong(song)
-    })
+    setSong(song) {
+      if (!song || song === this.state.song) {
+        return
+      }
+
+      this.setState({ song })
+
+      song.on("change", () => {
+        this.setState({ song })
+      })
+    }
+
+    render() {
+      return <WrappedComponent {...props} song={this.state.song} />
+    }
+  }
+}
+
+class Root extends Component {
+  componentDidMount() {
+    this.initKeyboardShortcut()
   }
 
   initKeyboardShortcut() {
@@ -36,25 +58,15 @@ export default class Root extends Component {
     }
   }
 
-  setSong(song) {
-    if (!song || song === this.state.song) {
-      return
-    }
-
-    this.setState({ song })
-
-    song.on("change", () => {
-      this.setState({ song })
-    })
-  }
-
   render() {
     const { app } = this.props
 
     return <RootView
       onChangeFile={file => app.open(file)}
       onSaveFile={() => app.save()}
-      song={this.state.song}
+      song={this.props.song}
     />
   }
 }
+
+export default withSong(Root)

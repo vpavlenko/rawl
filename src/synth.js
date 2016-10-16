e5fd5dc2a048a2a73685f1765e4bc32b10dc074a
ryohey <info@codingcafe.jp>
2016-10-16 22:48:39 +0900

    turn down the volume

--- src/synth.js
+++ src/synth.js
@@ -1,7 +1,7 @@
 import WebMidiLink from "./submodules/sf2synth/bin/sf2.synth.js"
 
 var wml = new WebMidiLink()
-wml.setLoadCallback(function(arraybuffer) {
-  // ロード完了時の処理
+wml.setLoadCallback(function() {
+  wml.synth.setMasterVolume(16384 * 0.5)
 })
 wml.setup("./soundfonts/msgs.sf2")

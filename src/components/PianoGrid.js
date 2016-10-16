bacee7389735c8d0d3bc3aa5d652dfb2f975ec17
ryohey <info@codingcafe.jp>
2016-10-16 23:02:42 +0900

    draw only visible area

--- src/components/PianoGrid.js
+++ src/components/PianoGrid.js
@@ -4,14 +4,16 @@ import DrawCanvas from "./DrawCanvas"
 import withTheme from "../hocs/withTheme"
 import logEq from "../helpers/logEq"
 
-function drawBeatLines(ctx, transform, endTick, ticksPerBeat, theme) {
+function drawBeatLines(ctx, transform, startTick, endTick, ticksPerBeat, theme) {
   const height = transform.getMaxY()
   const pixelsPerTick = transform.pixelsPerTick
   ctx.lineWidth = 1
+  const startBeat = Math.floor(startTick / ticksPerBeat)
+  const endBeat = Math.floor(endTick / ticksPerBeat)
 
-  for (let beats = 0; beats < endTick / ticksPerBeat; beats++) {
+  for (let beats = startBeat; beats < endBeat; beats++) {
     const x = beats * ticksPerBeat * pixelsPerTick
-    const isBold = beats % 4 == 0
+    const isBold = beats % 4 === 0
     ctx.beginPath()
     ctx.strokeStyle = isBold ? theme.secondaryTextColor : theme.dividerColor
     ctx.moveTo(x, 0)
@@ -22,36 +24,43 @@ function drawBeatLines(ctx, transform, endTick, ticksPerBeat, theme) {
 }
 
 function PianoGrid(props) {
-  const { transform } = props
+  const { transform, width, endTick, scrollLeft, ticksPerBeat, theme } = props
+
   function draw(ctx) {
     const { width, height } = ctx.canvas
     ctx.clearRect(0, 0, width, height)
     ctx.save()
-    ctx.translate(0.5, 0.5)
-    drawBeatLines(ctx, transform, props.endTick, props.ticksPerBeat, props.theme)
+    ctx.translate(-scrollLeft + 0.5, 0)
+    const startTick = transform.getTicks(scrollLeft)
+    drawBeatLines(ctx, transform, startTick, endTick, ticksPerBeat, theme)
     ctx.restore()
   }
 
   return <DrawCanvas
     draw={draw}
     className="PianoGrid"
-    width={transform.pixelsPerTick * props.endTick}
+    width={width}
     height={transform.pixelsPerKey * transform.numberOfKeys}
   />
 }
 
 PianoGrid.propTypes = {
+  width: PropTypes.number.isRequired,
   endTick: PropTypes.number.isRequired,
-  transform: PropTypes.object.isRequired,
+  scrollLeft: PropTypes.number.isRequired,
   ticksPerBeat: PropTypes.number.isRequired,
+  theme: PropTypes.object.isRequired,
+  transform: PropTypes.object.isRequired
 }
 
 class _PianoGrid extends Component {
   shouldComponentUpdate(nextProps) {
     const props = this.props
     return !logEq(props, nextProps, "theme", _.isEqual)
-      || !logEq(props, nextProps, "ticksPerBeat", (x, y) => x === y)
+      || !logEq(props, nextProps, "width", (x, y) => x === y)
       || !logEq(props, nextProps, "endTick", (x, y) => x === y)
+      || !logEq(props, nextProps, "scrollLeft", (x, y) => x === y)
+      || !logEq(props, nextProps, "ticksPerBeat", (x, y) => x === y)
       || !logEq(props, nextProps, "transform", (x, y) => x && y && x.equals(y))
   }
 
--- src/components/PianoRuler.js
+++ src/components/PianoRuler.js
@@ -4,14 +4,14 @@ import DrawCanvas from "./DrawCanvas"
 import withTheme from "../hocs/withTheme"
 import pureRender from "../hocs/pureRender"
 
-const theme = Theme.fromCSS()
-
-function drawRuler(ctx, height, pixelsPerTick, endTick, ticksPerBeat, theme) {
+function drawRuler(ctx, height, pixelsPerTick, startTick, endTick, ticksPerBeat, theme) {
   ctx.beginPath()
   ctx.strokeStyle = theme.secondaryTextColor
   ctx.lineWidth = 1
+  const startBeat = Math.floor(startTick / ticksPerBeat)
+  const endBeat = Math.floor(endTick / ticksPerBeat)
 
-  for (let beats = 0; beats < endTick / ticksPerBeat; beats++) {
+  for (let beats = startBeat; beats < endBeat; beats++) {
     const x = beats * ticksPerBeat * pixelsPerTick
     const isTop = beats % 4 == 0
 
@@ -37,30 +37,36 @@ function drawRuler(ctx, height, pixelsPerTick, endTick, ticksPerBeat, theme) {
 }
 
 function PianoRuler(props) {
+  const { height, transform, pixelsPerTick, endTick, scrollLeft, ticksPerBeat, theme, onMouseDown } = props
+
   function draw(ctx) {
     const { width, height } = ctx.canvas
     ctx.clearRect(0, 0, width, height)
     ctx.save()
-    ctx.translate(0.5, 0.5)
-    drawRuler(ctx, props.height, props.pixelsPerTick, props.endTick, props.ticksPerBeat, props.theme)
+    ctx.translate(-scrollLeft + 0.5, 0)
+    const startTick = transform.getTicks(scrollLeft)
+    drawRuler(ctx, height, pixelsPerTick, startTick, endTick, ticksPerBeat, theme)
     ctx.restore()
   }
 
   return <DrawCanvas
     draw={draw}
     className="PianoRuler"
-    width={props.pixelsPerTick * props.endTick}
-    height={props.height}
-    style={props.style}
-    onMouseDown={props.onMouseDown}
+    width={pixelsPerTick * endTick}
+    height={height}
+    onMouseDown={onMouseDown}
   />
 }
 
 PianoRuler.propTypes = {
   pixelsPerTick: PropTypes.number.isRequired,
   ticksPerBeat: PropTypes.number.isRequired,
+  scrollLeft: PropTypes.number.isRequired,
   endTick: PropTypes.number.isRequired,
-  height: PropTypes.number.isRequired
+  height: PropTypes.number.isRequired,
+  transform: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+  onMouseDown: PropTypes.func
 }
 
 export default pureRender(withTheme(PianoRuler))

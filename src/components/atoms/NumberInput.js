9f53bcc0673eba97aec2a63b6b1da01642a522a1
ryohey <info@codingcafe.jp>
2017-01-19 22:17:09 +0900

    support drag NumberInput to change value

--- src/components/atoms/NumberInput.js
+++ src/components/atoms/NumberInput.js
@@ -4,11 +4,39 @@ import f from "../../helpers/flatJoin"
 import "./NumberInput.css"
 
 export default function NumberInput({ className, value, onChange, placeholder }) {
+  function addValue(target, value) {
+    target.value = (parseFloat(target.value) || 0) + value
+    onChange(target)
+  }
+
+  function setValue(target, value) {
+    target.value = value
+    onChange(target)
+  }
+
   function handleWheel(e) {
     e.preventDefault()
     const movement = e.deltaY > 0 ? -1 : 1
-    e.target.value = (parseFloat(e.target.value) || 0) + movement
-    onChange(e)
+    addValue(e.target, movement)
+  }
+
+  function handleMouseDown(e) {
+    const { target } = e
+    const startY = e.clientY
+    const startValue = parseFloat(target.value) || 0
+
+    function onMouseMove(e) {
+      const delta = e.clientY - startY
+      setValue(target, startValue - Math.ceil(delta / 2))
+    }
+
+    function onMouseUp(e) {
+      window.removeEventListener("mousemove", onMouseMove)
+      window.removeEventListener("mouseup", onMouseUp)
+    }
+
+    window.addEventListener("mousemove", onMouseMove)
+    window.addEventListener("mouseup", onMouseUp)
   }
 
   return <input 
@@ -18,5 +46,6 @@ export default function NumberInput({ className, value, onChange, placeholder })
     onChange={onChange}
     placeholder={placeholder}
     onWheel={handleWheel}
+    onMouseDown={handleMouseDown}
   />
 }

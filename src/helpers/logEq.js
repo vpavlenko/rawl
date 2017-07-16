baca661848ffa8acf7a7faf01830683dd010904f
ryohey <info@codingcafe.jp>
2017-07-17 00:50:04 +0900

    remove log

--- src/helpers/logEq.js
+++ src/helpers/logEq.js
@@ -1,7 +1,3 @@
 export default function logEq(x, y, propName, compare = (a, b) => a === b) {
-  const result = compare(x[propName], y[propName])
-  if (!result) {
-    console.log(`${propName} is changed`)
-  }
-  return result
+  return compare(x[propName], y[propName])
 }

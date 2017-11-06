eeaee431176286e6feb0456cedf268dd360d55f9
ryohey <info@codingcafe.jp>
2017-11-06 14:04:08 +0900

    Add Recorder

--- src/synth.js
+++ src/synth.js
@@ -1,6 +1,8 @@
 import Synthesizer from "./submodules/sf2synth/src/sound_font_synth"
 import View from "./submodules/sf2synth/src/synth_view"
 import MidiMessageHandler from "./submodules/sf2synth/src/midi_message_handler"
+import Recorder from "./submodules/opus-recorder/src/recorder"
+
 import "./synth.css"
 
 const { ipcRenderer } = window.require("electron")
@@ -20,15 +22,31 @@ export default class SynthApp {
 
     const url = "/soundfonts/msgs.sf2"
     loadSoundFont(url, input => {
-      const synth = new Synthesizer(input)
+      const ctx = new window.AudioContext()
+      const output = ctx.createGain()
+      output.connect(ctx.destination)
+
+      const synth = new Synthesizer(input, ctx)
       synth.init()
-      synth.start()
+      synth.connect(output)
       handler.synth = synth
 
       const view = new View()
       synth.view = view
       document.body.classList.add("synth")
       document.getElementById("root").appendChild(view.draw(synth))
+
+      document.getElementById("root").insertAdjacentHTML("beforeend", `
+      <h2>Commands</h2>
+      <button id="init">init</button>
+      <button id="start" disabled>start</button>
+      <button id="pause" disabled>pause</button>
+      <button id="resume" disabled>resume</button>
+      <button id="stopButton" disabled>stop</button>
+    
+      <h2>Recordings</h2>
+      <ul id="recordingslist"></ul>`)
+      prepareRecorder(ctx, output)
     })
 
     const onTimer = () => {
@@ -47,6 +65,74 @@ export default class SynthApp {
 
     onTimer()
   }
+}
+
+function prepareRecorder(ctx, output) {
+  const recorder = new Recorder(ctx, {
+    encoderPath: "/libs/opus-recorder/waveWorker.min.js"
+  })
+  const init = document.querySelector("#init")
+  const start = document.querySelector("#start")
+  const pause = document.querySelector("#pause")
+  const resume = document.querySelector("#resume")
+  const stopButton = document.querySelector("#stopButton")
+  const recordingslist = document.querySelector("#recordingslist")
+
+  init.addEventListener("click", () => initRecorder())
+  start.addEventListener("click", () => { recorder.start() })
+  pause.addEventListener("click", () => { recorder.pause() })
+  resume.addEventListener("click", () => { recorder.resume() })
+  stopButton.addEventListener("click", () => { recorder.stop() })
+
+  recorder.addEventListener("start", e => {
+    console.log('Recorder is started')
+    start.disabled = resume.disabled = true
+    pause.disabled = stopButton.disabled = false
+  })
+  recorder.addEventListener("stop", e => {
+    console.log('Recorder is stopped')
+    pause.disabled = resume.disabled = stopButton.disabled = start.disabled = true
+  })
+  recorder.addEventListener("pause", e => {
+    console.log('Recorder is paused')
+    pause.disabled = start.disabled = true
+    resume.disabled = stopButton.disabled = false
+  })
+  recorder.addEventListener("resume", e => {
+    console.log('Recorder is resuming')
+    start.disabled = resume.disabled = true
+    pause.disabled = stopButton.disabled = false
+  })
+  recorder.addEventListener("streamError", e => {
+    console.log('Error encountered: ' + e.error.name)
+  })
+  recorder.addEventListener("streamReady", e => {
+    pause.disabled = resume.disabled = stopButton.disabled = true
+    start.disabled = false
+    console.log('Audio stream is ready.')
+  })
+  recorder.addEventListener("dataAvailable", e => {
+    var dataBlob = new Blob([e.detail], { type: 'audio/wav' })
+    var fileName = new Date().toISOString() + ".wav"
+    var url = URL.createObjectURL(dataBlob)
+    var audio = document.createElement('audio')
+    audio.controls = true
+    audio.src = url
+    var link = document.createElement('a')
+    link.href = url
+    link.download = fileName
+    link.innerHTML = link.download
+    var li = document.createElement('li')
+    li.appendChild(link)
+    li.appendChild(audio)
+    recordingslist.appendChild(li)
+  })
+
+  function initRecorder() {
+    recorder.initStream(output)
+  }
+
+  return recorder
 }
 
 function loadSoundFont(url, callback) {

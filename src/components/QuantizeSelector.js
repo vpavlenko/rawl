af145e6f2df1306a1ada1aa6b74571cc17693072
ryohey <info@codingcafe.jp>
2018-03-04 14:55:45 +0900

    Add QuantizePopup

--- src/components/QuantizeSelector.js
+++ src/components/QuantizeSelector.js
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { Component } from "react"
 import Icon from "components/Icon"
+import QuantizePopup from "components/QuantizePopup"
 
 import "./QuantizeSelector.css"
 
@@ -24,7 +25,7 @@ function TripletButton({ selected, onClick }) {
   return <div className={`triplet button ${selected ? "selected" : ""}`} onClick={onClick}>3</div>
 }
 
-export default function QuantizeSelector({ value, onSelect }) {
+function QuantizeSelector({ value, onSelect, popupHidden, togglePopup }) {
   // 整数ではなく 1.5 をかけると整数になるとき付点
   const dot = (value % 1 !== 0) && ((value * 1.5) % 1 === 0)
 
@@ -34,7 +35,7 @@ export default function QuantizeSelector({ value, onSelect }) {
   // 逆算するために triplet と dot を逆にする
   const denominator = calcQuantize(value, triplet, dot)
 
-  const list = [1, 2, 4, 8, 16, 32]
+  const list = [1, 2, 4, 8, 16, 32, 64, 128]
 
   return <div className="QuantizeSelector" onWheel={e => {
     const currentIndex = list.indexOf(denominator)
@@ -42,23 +43,40 @@ export default function QuantizeSelector({ value, onSelect }) {
     const index = Math.min(list.length - 1, Math.max(0, currentIndex + delta))
     onSelect(calcQuantize(list[index], dot, triplet))
   }}>
-    <label className="label">
-      <Icon>music-note</Icon>
-    </label>
-    <select
-      className="denominator-select"
+    <div className="content" onClick={togglePopup}>
+      <Icon className="label">music-note</Icon>
+      <div className="value">
+        <span className="denominator">{denominator}</span>
+        {triplet && <span className="triplet-label">3</span>}
+        {dot && <Icon className="dot-label">circle</Icon>}
+      </div>
+    </div>
+    <QuantizePopup 
+      hidden={popupHidden}
       value={denominator}
-      onChange={e => onSelect(calcQuantize(parseInt(e.target.value, 10), dot, triplet))}>
-      {list.map(num =>
-        <option key={num} value={num}>{num}</option>
-      )}
-    </select>
-    <div className="updown-mark">
-      <Icon>chevron-down</Icon>
-    </div>
-    <div className="options">
-      <DotButton selected={dot} onClick={() => onSelect(calcQuantize(denominator, !dot, false))} />
-      <TripletButton selected={triplet} onClick={() => onSelect(calcQuantize(denominator, false, !triplet))} />
-    </div>
+      values={list}
+      dotted={dot}
+      triplet={triplet}
+      onChangeValue={d => onSelect(calcQuantize(d, dot, triplet))} 
+      onChangeDotted={d => onSelect(calcQuantize(denominator, d, false))}
+      onChangeTriplet={t => onSelect(calcQuantize(denominator, false, t)) }
+    />
   </div>
+}
+
+
+export default class stateful extends Component {
+  constructor() {
+    super()
+    this.state = {
+      popupHidden: true
+    }
+  }
+
+  render() {
+    return <QuantizeSelector 
+      togglePopup={() => this.setState({ popupHidden: !this.state.popupHidden })}
+      {...this.state} 
+      {...this.props} />
+  }
 }

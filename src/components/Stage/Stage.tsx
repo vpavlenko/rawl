4fb0c842353525f22f517e6423def926d25d016e
ryohey <info@codingcafe.jp>
2018-03-31 21:08:15 +0900

    Rewrite Stage in TypeScript

--- src/components/Stage/Stage.tsx
+++ src/components/Stage/Stage.tsx
@@ -1,7 +1,30 @@
-import React from "react"
+import React, { MouseEvent } from "react"
 
 import DrawCanvas from "components/DrawCanvas.tsx"
-import Rect from "model/Rect"
+import { IRect, containsPoint as rectContainsPoint, intersects as rectIntersects } from "model/Rect"
+import Item from "./Item"
+import { IPoint } from "model/Point"
+
+interface ItemEvent extends MouseEvent<HTMLElement> {
+  items: Item[]
+  local: IPoint
+}
+
+export interface StageProps {
+  items: Item[]
+  onMouseDown: (ItemEvent) => void
+  onMouseMove: (ItemEvent) => void
+  onMouseUp: (ItemEvent) => void
+  onWheel: (ItemEvent) => void
+  onDoubleClick: (ItemEvent) => void
+  onContextMenu: (ItemEvent) => void
+  width: number
+  height: number
+  scrollLeft: number
+  scrollTop: number
+  className: string
+  style: any
+}
 
 /**
  * Item の描画、マウスイベントのハンドリングを実装した Canvas
@@ -20,7 +43,7 @@ export default function Stage({
   scrollTop = 0,
   className = "",
   style
-}) {
+}: StageProps) {
   function draw(ctx) {
     const { width, height } = ctx.canvas
     ctx.clearRect(0, 0, width, height)
@@ -32,30 +55,34 @@ export default function Stage({
   }
 
   function drawItems(ctx) {
-    const viewRect = new Rect(scrollLeft, scrollTop, width, height)
-    const displayedItems = items.filter(item => viewRect.intersects(item.bounds))
+    const viewRect = { x: scrollLeft, y: scrollTop, width, height }
+    const displayedItems = items.filter(item => rectIntersects(viewRect, item.bounds))
     displayedItems.forEach(item => item.render(ctx))
   }
 
   let isMouseDown = false
 
-  function onMouseDown(e) {
+  function onMouseDown(e: MouseEvent<HTMLElement>) {
     e.nativeEvent.preventDefault()
     isMouseDown = true
 
-    let { left, top } = e.target.getBoundingClientRect()
+    let { left, top } = e.currentTarget.getBoundingClientRect()
     left -= scrollLeft
     top -= scrollTop
     const startPos = {
       x: e.nativeEvent.offsetX + scrollLeft,
       y: e.nativeEvent.offsetY + scrollTop
     }
-    const clickedItems = items.filter(item => item.bounds.containsPoint(startPos))
+    const clickedItems = items.filter(item => rectContainsPoint(item.bounds, startPos))
 
     function onMouseMove(e) {
       e.preventDefault()
       const local = { x: e.clientX - left, y: e.clientY - top }
-      _onMouseMove(Object.assign(e, { items: clickedItems, local }))
+      _onMouseMove({
+        ...e, 
+        items: clickedItems, 
+        local
+      })
     }
 
     function onMouseUp(e) {
@@ -64,21 +91,29 @@ export default function Stage({
       document.removeEventListener("mouseup", onMouseUp)
 
       const local = { x: e.clientX - left, y: e.clientY - top }
-      _onMouseUp(Object.assign(e, { items: clickedItems, local }))
+      _onMouseUp({
+        ...e, 
+        items: clickedItems,
+        local 
+      })
       isMouseDown = false
     }
 
     document.addEventListener("mousemove", onMouseMove)
     document.addEventListener("mouseup", onMouseUp)
 
-    _onMouseDown(Object.assign(e, { items: clickedItems, local: startPos }))
+    _onMouseDown({
+      ...e, 
+      items: clickedItems, 
+      local: startPos
+    })
   }
 
   function onWheel(e) {
     _onWheel(extendEvent(e))
   }
 
-  function onMouseMoveCanvas(e) {
+  function onMouseMoveCanvas(e: MouseEvent<HTMLElement>) {
     // ドラッグ中はウィンドウ外も扱うため document の eventListener から呼ぶが、
     // そうでないときはここから呼ぶ
     if (!isMouseDown) {
@@ -86,14 +121,17 @@ export default function Stage({
     }
   }
 
-  function extendEvent(e) {
+  function extendEvent(e: MouseEvent<HTMLElement>): ItemEvent {
     const local = {
       x: e.nativeEvent.offsetX + scrollLeft,
       y: e.nativeEvent.offsetY + scrollTop
     }
-    const hitItems = items.filter(item => item.bounds.containsPoint(local))
-    Object.assign(e, { items: hitItems, local })
-    return e
+    const hitItems = items.filter(item => rectContainsPoint(item.bounds, local))
+    return {
+      ...e,
+      items: hitItems, 
+      local
+    }
   }
 
   return <DrawCanvas

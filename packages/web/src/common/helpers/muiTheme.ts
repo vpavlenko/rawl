63e9552323f4bab12d7ec047a77e22bc5576cd5a
ryohey <info@codingcafe.jp>
2019-10-09 10:36:06 +0900

    Readability improvements

--- packages/web/src/common/helpers/muiTheme.ts
+++ packages/web/src/common/helpers/muiTheme.ts
@@ -10,5 +10,19 @@ export const theme = createMuiTheme({
       default: "#454b60",
       paper: "#272a36"
     }
+  },
+  typography: {
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"'
+    ].join(",")
   }
 })

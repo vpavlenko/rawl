702212ab7251d9291b4f98949ef83e17dd3180cf
ryohey <info@codingcafe.jp>
2019-10-09 09:04:44 +0900

    Fix styles

--- packages/web/src/common/helpers/muiTheme.ts
+++ packages/web/src/common/helpers/muiTheme.ts
@@ -4,8 +4,11 @@ export const theme = createMuiTheme({
   palette: {
     type: "dark",
     primary: {
-      main: "hsl(230, 70%, 55%)",
-      contrastText: "#fbfcff"
+      main: "hsl(230, 70%, 55%)"
+    },
+    background: {
+      default: "#454b60",
+      paper: "#272a36"
     }
   }
 })

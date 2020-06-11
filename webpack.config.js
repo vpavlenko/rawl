6e3059efb619beb474e7d4fd1b428b269a0d10bb
ryohey <info@codingcafe.jp>
2020-06-11 18:40:45 +0900

    Add bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin")
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin
 
 module.exports = {
   context: __dirname,
   mode: "development",
   entry: {
     browserMain: "./src/main/index.tsx",
-    browserSynth: "./src/synth/index.tsx"
+    browserSynth: "./src/synth/index.tsx",
   },
   output: {
-    filename: "[name].js"
+    filename: "[name].js",
   },
   devServer: {
     contentBase: path.join(__dirname, "public"),
@@ -19,9 +21,9 @@ module.exports = {
     watchContentBase: true,
     hotOnly: true,
     overlay: {
-      warnings: true,
-      errors: true
-    }
+      warnings: false,
+      errors: true,
+    },
   },
   devtool: "inline-source-map",
   module: {
@@ -29,42 +31,43 @@ module.exports = {
       {
         test: /\.jsx?$/,
         use: "babel-loader",
-        exclude: [/node_modules/]
+        exclude: [/node_modules/],
       },
       {
         test: /\.(j|t)sx?$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|eot|ttf)$/,
-        loader: "url-loader"
-      }
-    ]
+        loader: "url-loader",
+      },
+    ],
   },
   resolve: {
     modules: ["src", "node_modules", "src/main", "src/common"],
-    extensions: [".js", ".jsx", ".ts", ".tsx"]
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
   plugins: [
+    new BundleAnalyzerPlugin(),
     new ForkTsCheckerWebpackPlugin(),
     new HtmlWebpackPlugin({
       inject: true,
       filename: "main.html",
       chunks: ["browserMain"],
-      template: path.join(__dirname, "public", "main.html")
+      template: path.join(__dirname, "public", "main.html"),
     }),
     new HtmlWebpackPlugin({
       inject: true,
       filename: "synth.html",
       chunks: ["browserSynth"],
-      template: path.join(__dirname, "public", "synth.html")
-    })
-  ]
+      template: path.join(__dirname, "public", "synth.html"),
+    }),
+  ],
 }

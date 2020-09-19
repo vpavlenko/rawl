a5cecb0b3b02eba90410b3104fd0d0e84cae66bf
ryohey <info@codingcafe.jp>
2020-09-19 15:35:46 +0900

    Update deps and install ESLint

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,4 @@
-const merge = require("webpack-merge")
+const { merge } = require("webpack-merge")
 const common = require("./webpack.common.js")
 const path = require("path")
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin")

15cf0c3ddd07d8a5e8afb04c66c6ff456681b964
ryohey <info@codingcafe.jp>
2020-09-26 22:39:44 +0900

    Show dialog when leave

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -7,6 +7,11 @@ function renderApp() {
   ReactDOM.render(<App />, document.querySelector("#root"))
 }
 
+window.onbeforeunload = (e: BeforeUnloadEvent) => {
+  e.returnValue =
+    "Your edits have not been saved. Be sure to download it before exiting. Do you really want to close it?"
+}
+
 renderApp()
 
 if (module.hot) {

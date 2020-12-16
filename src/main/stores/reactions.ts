9644e8a850cdd8c785baf3d584478e75f9150639
ryohey <info@codingcafe.jp>
2020-12-16 10:39:05 +0900

    Refactor

--- src/main/stores/reactions.ts
+++ src/main/stores/reactions.ts
@@ -1,6 +1,5 @@
-import { autorun, observe } from "mobx"
+import { observe } from "mobx"
 import { isNotNull, isNotUndefined } from "../../common/helpers/array"
-import { emptySelection } from "../../common/selection/Selection"
 import { resetSelection } from "../actions"
 import MIDIOutput from "../services/MIDIOutput"
 import RootStore from "./RootStore"
@@ -9,56 +8,63 @@ export const registerReactions = (rootStore: RootStore) => {
   // reset selection when tool changed
   observe(rootStore.pianoRollStore, "mouseMode", resetSelection(rootStore))
 
-  // sync synthGroup.output to enabledOutputIds/isFactorySoundEnabled
-  const updateOutputDevices = () => {
-    const { midiDeviceStore } = rootStore
+  observe(
+    rootStore.midiDeviceStore,
+    "enabledOutputIds",
+    updateOutputDevices(rootStore)
+  )
 
-    rootStore.services.player.allSoundsOff()
-
-    const getMIDIDeviceEntries = () =>
-      Array.from(midiDeviceStore.enabledOutputIds.values())
-        .map((deviceId) => {
-          const device = midiDeviceStore.outputs.find((d) => d.id === deviceId)
-          if (device === undefined) {
-            console.error(`device not found: ${deviceId}`)
-            return null
-          }
-          return { synth: new MIDIOutput(device), isEnabled: true }
-        })
-        .filter(isNotNull)
-
-    rootStore.services.synthGroup.outputs = [
-      {
-        synth: rootStore.services.synth,
-        isEnabled: midiDeviceStore.isFactorySoundEnabled,
-      },
-      ...getMIDIDeviceEntries(),
-    ]
-  }
-
-  observe(rootStore.midiDeviceStore, "enabledOutputIds", updateOutputDevices)
   observe(
     rootStore.midiDeviceStore,
     "isFactorySoundEnabled",
-    updateOutputDevices
+    updateOutputDevices(rootStore)
   )
 
-  const updateInputDevices = () => {
-    const { midiDeviceStore } = rootStore
-
-    const devices = Array.from(midiDeviceStore.enabledInputIds.values())
-      .map((deviceId) => midiDeviceStore.inputs.find((d) => d.id === deviceId))
-      .filter(isNotUndefined)
-
-    rootStore.services.midiInputGroup.devices = devices
-  }
-
-  observe(rootStore.midiDeviceStore, "enabledInputIds", updateInputDevices)
+  observe(
+    rootStore.midiDeviceStore,
+    "enabledInputIds",
+    updateInputDevices(rootStore)
+  )
 
   // reset selection when change track
-  autorun(() => {
-    // deep observe
-    const _ = rootStore.song.selectedTrackId
-    rootStore.pianoRollStore.selection = emptySelection
-  })
+  observe(rootStore.song, "selectedTrackId", resetSelection(rootStore))
+}
+
+type Reaction = (rootStore: RootStore) => () => void
+
+// sync synthGroup.output to enabledOutputIds/isFactorySoundEnabled
+const updateOutputDevices: Reaction = (rootStore) => () => {
+  const { midiDeviceStore } = rootStore
+
+  rootStore.services.player.allSoundsOff()
+
+  const getMIDIDeviceEntries = () =>
+    Array.from(midiDeviceStore.enabledOutputIds.values())
+      .map((deviceId) => {
+        const device = midiDeviceStore.outputs.find((d) => d.id === deviceId)
+        if (device === undefined) {
+          console.error(`device not found: ${deviceId}`)
+          return null
+        }
+        return { synth: new MIDIOutput(device), isEnabled: true }
+      })
+      .filter(isNotNull)
+
+  rootStore.services.synthGroup.outputs = [
+    {
+      synth: rootStore.services.synth,
+      isEnabled: midiDeviceStore.isFactorySoundEnabled,
+    },
+    ...getMIDIDeviceEntries(),
+  ]
+}
+
+const updateInputDevices: Reaction = (rootStore) => () => {
+  const { midiDeviceStore } = rootStore
+
+  const devices = Array.from(midiDeviceStore.enabledInputIds.values())
+    .map((deviceId) => midiDeviceStore.inputs.find((d) => d.id === deviceId))
+    .filter(isNotUndefined)
+
+  rootStore.services.midiInputGroup.devices = devices
 }

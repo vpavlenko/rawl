3cb17a64a36b72aef8fe5ed8618d076db3b117b1
ryohey <info@codingcafe.jp>
2020-12-24 13:30:28 +0900

    Remove css from EventEditor

--- src/main/components/EventEditor/EventEditor.tsx
+++ src/main/components/EventEditor/EventEditor.tsx
@@ -9,20 +9,20 @@ import { useObserver } from "mobx-react-lite"
 import React, { FC } from "react"
 import { localized } from "../../../common/localize/localizedString"
 import { useStores } from "../../hooks/useStores"
-import "./EventEditor.css"
 import EventList from "./EventList"
 
 export const EventEditor: FC = () => {
   const { rootViewStore } = useStores()
+  const rootStore = useStores()
   const { isOpen, events } = useObserver(() => ({
     isOpen: rootViewStore.openEventEditor,
-    events: rootViewStore.eventEditorEvents,
+    events: rootStore.song?.selectedTrack?.events ?? [],
   }))
   const close = () => (rootViewStore.openEventEditor = false)
   const onClickOK = close
   const onClickCancel = close
   return (
-    <Dialog open={isOpen} onClose={close} className="EventEditor">
+    <Dialog open={isOpen} onClose={close}>
       <DialogTitle>{localized("midi-settings", "MIDI Settings")}</DialogTitle>
       <DialogContent>
         <EventList events={events} />

fca7b11ce1fb87bf9564331cfc4f72da2ba70b13
ryohey <info@codingcafe.jp>
2021-03-26 21:28:03 +0900

    Remove PianoNotes

---
+++ src/main/hooks/useNotes.ts
@@ -0,0 +1,46 @@
+import { useObserver } from "mobx-react-lite"
+import { useMemo } from "react"
+import { filterEventsWithScroll } from "../../common/helpers/filterEventsWithScroll"
+import { isNoteEvent, NoteEvent } from "../../common/track"
+import { PianoNoteItem } from "../components/PianoRoll/PianoNotes/PianoNote"
+import { useMemoObserver } from "./useMemoObserver"
+import { useNoteTransform } from "./useNoteTransform"
+import { useStores } from "./useStores"
+
+export const useNotes = (trackId: number, width: number, isGhost: boolean) => {
+  const rootStore = useStores()
+
+  const { events, isRhythmTrack } = useObserver(() => {
+    const track = rootStore.song.tracks[trackId]
+    return {
+      events: [...(track?.events ?? [])], // create new object to fire useMemo update
+      isRhythmTrack: track?.isRhythmTrack ?? false,
+    }
+  })
+
+  const scrollLeft = useMemoObserver(() => rootStore.pianoRollStore.scrollLeft)
+  const selection = useObserver(() => rootStore.pianoRollStore.selection)
+
+  const transform = useNoteTransform()
+
+  const windowNotes = (notes: NoteEvent[]): NoteEvent[] =>
+    filterEventsWithScroll(notes, transform.pixelsPerTick, scrollLeft, width)
+
+  return useMemo(
+    () =>
+      windowNotes(events.filter(isNoteEvent)).map(
+        (e): PianoNoteItem => {
+          const rect = transform.getRect(e)
+          const isSelected = !isGhost && selection.noteIds.includes(e.id)
+          return {
+            ...rect,
+            id: e.id,
+            velocity: isGhost ? 127 : e.velocity, // draw opaque when ghost
+            isSelected,
+            isDrum: isRhythmTrack,
+          }
+        }
+      ),
+    [events, transform, scrollLeft, width, selection, isGhost, isRhythmTrack]
+  )
+}

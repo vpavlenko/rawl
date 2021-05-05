e76b240304b90b567c64b77fd4aae34ebd85207c
ryohey <info@codingcafe.jp>
2021-05-05 22:21:48 +0900

    Add Renderer2D

--- src/main/components/PianoRoll/PianoRollRenderer/PianoRollRenderer.ts
+++ src/main/components/PianoRoll/PianoRollRenderer/PianoRollRenderer.ts
@@ -1,16 +1,11 @@
-//
-// Initialize a shader program, so WebGL knows how to draw our data
-
 import Color from "color"
-import { mat4, vec3, vec4 } from "gl-matrix"
+import { vec4 } from "gl-matrix"
 import { partition } from "lodash"
-import { ISize } from "pixi.js"
-import { IPoint, IRect, zeroPoint, zeroRect } from "../../../../common/geometry"
+import { IPoint, IRect } from "../../../../common/geometry"
 import { defaultTheme, Theme } from "../../../../common/theme/Theme"
 import { Layout } from "../../../Constants"
 import { colorToVec4 } from "../../../gl/color"
-import { DisplayObject } from "../../../gl/DisplayObject"
-import { RenderProperty } from "../../../gl/RenderProperty"
+import { Renderer2D, translateMatrix } from "../../../gl/Renderer2D"
 import { BorderedCircleObject } from "../../../gl/shaders/BorderedCircleShader"
 import { BorderedRectangleObject } from "../../../gl/shaders/BorderedRectangleShader"
 import { SolidRectangleObject } from "../../../gl/shaders/SolidRectangleShader"
@@ -20,24 +15,13 @@ import { HorizontalGridObject } from "./HorizontalGridShader"
 import { NoteObject } from "./NoteShader"
 
 export class PianoRollRenderer {
-  private gl: WebGLRenderingContext
-
-  private viewSize: RenderProperty<ISize> = new RenderProperty(
-    { width: 0, height: 0 },
-    (a, b) => a.width === b.width && a.height === b.height
-  )
-
-  private objects: DisplayObject<any, any>[] = []
+  private renderer: Renderer2D
 
   private noteObject: NoteObject
   private hGridObject: HorizontalGridObject
-
   private drumNoteObject: DrumNoteObject
-
   private ghostNoteObject: BorderedRectangleObject
-
   private ghostDrumNoteObject: BorderedCircleObject
-
   private selectionObject: BorderedRectangleObject
   private beatObject: SolidRectangleObject
   private highlightedBeatObject: SolidRectangleObject
@@ -46,7 +30,7 @@ export class PianoRollRenderer {
   theme: Theme = defaultTheme
 
   constructor(gl: WebGLRenderingContext) {
-    this.gl = gl
+    this.renderer = new Renderer2D(gl)
 
     this.hGridObject = new HorizontalGridObject(gl)
     this.beatObject = new SolidRectangleObject(gl)
@@ -58,7 +42,7 @@ export class PianoRollRenderer {
     this.ghostDrumNoteObject = new BorderedCircleObject(gl)
     this.selectionObject = new BorderedRectangleObject(gl)
 
-    this.objects.push(
+    const objects = [
       // this.hGridObject,
       this.beatObject,
       this.highlightedBeatObject,
@@ -67,17 +51,17 @@ export class PianoRollRenderer {
       this.noteObject,
       this.drumNoteObject,
       this.selectionObject,
-      this.cursorObject
-    )
+      this.cursorObject,
+    ]
 
-    this.render([], [], zeroRect, [], [], 0, zeroPoint)
+    objects.forEach((o) => this.renderer.addObject(o))
   }
 
   private vline = (x: number): IRect => ({
     x,
     y: 0,
     width: 1,
-    height: this.viewSize.value.height,
+    height: this.renderer.gl.canvas.height,
   })
 
   render(
@@ -105,86 +89,24 @@ export class PianoRollRenderer {
     this.selectionObject.updateBuffer([selection])
     this.beatObject.updateBuffer(beats.map(this.vline))
     this.highlightedBeatObject.updateBuffer(highlightedBeats.map(this.vline))
-    this.hGridObject.updateBuffer(this.viewSize.value)
+    this.hGridObject.updateBuffer(this.renderer.gl.canvas)
     this.cursorObject.updateBuffer([this.vline(cursorX)])
 
     this.updateUniforms(scroll)
-    this.draw()
-  }
-
-  private clear() {
-    const { gl } = this
-    gl.clearColor(0.0, 0.0, 0.0, 0.0)
-    gl.clearDepth(1.0)
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-  }
-
-  private draw() {
-    const { gl } = this
-
-    this.viewSize.value = {
-      width: gl.canvas.width,
-      height: gl.canvas.height,
-    }
-
-    if (this.viewSize.isDirty) {
-      gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
-    }
-
-    gl.disable(gl.CULL_FACE)
-    gl.disable(gl.DEPTH_TEST)
-    gl.disable(gl.DITHER)
-    gl.disable(gl.POLYGON_OFFSET_FILL)
-    gl.disable(gl.SAMPLE_ALPHA_TO_COVERAGE)
-    gl.disable(gl.SAMPLE_COVERAGE)
-    gl.disable(gl.SCISSOR_TEST)
-    gl.disable(gl.STENCIL_TEST)
-
-    gl.enable(gl.BLEND)
-    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
-
-    this.clear()
-
-    this.objects.forEach((o) => o.draw())
+    this.renderer.render()
   }
 
   private updateUniforms(scroll: IPoint) {
-    const { gl } = this
-    const zNear = 0
-    const zFar = 100.0
-    const projectionMatrix = mat4.create()
-
-    const canvas = gl.canvas as HTMLCanvasElement
-
-    const scale = canvas.clientWidth / canvas.width
-    mat4.scale(
+    const projectionMatrix = this.renderer.createProjectionMatrix()
+    const projectionMatrixScrollX = translateMatrix(
       projectionMatrix,
-      projectionMatrix,
-      vec3.fromValues(scale, scale, scale)
+      -scroll.x,
+      0
     )
-
-    mat4.ortho(
+    const projectionMatrixScrollXY = translateMatrix(
       projectionMatrix,
-      0,
-      canvas.clientWidth,
-      canvas.clientHeight,
-      0,
-      zNear,
-      zFar
-    )
-
-    const projectionMatrixScrollX = mat4.create()
-    mat4.translate(
-      projectionMatrixScrollX,
-      projectionMatrix,
-      vec3.fromValues(-scroll.x, 0, 0)
-    )
-
-    const projectionMatrixScrollXY = mat4.create()
-    mat4.translate(
-      projectionMatrixScrollXY,
-      projectionMatrix,
-      vec3.fromValues(-scroll.x, -scroll.y, 0)
+      -scroll.x,
+      -scroll.y
     )
 
     {

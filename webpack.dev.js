238c4875d26ef8cc720dfb23fc0dccd50ba5d75e
ryohey <info@codingcafe.jp>
2021-05-11 12:56:16 +0900

    Enable source-map on production

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,7 +6,6 @@ const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin"
 
 module.exports = merge(common, {
   mode: "development",
-  devtool: "inline-source-map",
   devServer: {
     contentBase: path.join(__dirname, "public"),
     port: 3000,

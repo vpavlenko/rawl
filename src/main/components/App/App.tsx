f2ca7dcb573c71b8a7a4c298cbbeb4702e0eb1fd
ryohey <info@codingcafe.jp>
2021-07-28 15:31:30 +0900

    Fix sentry init

--- src/main/components/App/App.tsx
+++ src/main/components/App/App.tsx
@@ -14,6 +14,7 @@ import { GlobalKeyboardShortcut } from "../KeyboardShortcut/GlobalKeyboardShortc
 import { RootView } from "../RootView/RootView"
 
 Sentry.init({
+  dsn: process.env.SENTRY_DSN,
   release: process.env.VERCEL_GIT_COMMIT_SHA,
   environment: process.env.VERCEL_ENV,
   integrations: [new Integrations.BrowserTracing()],

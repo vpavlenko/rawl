ce4388c4f9b9a77f6199d3873c8ed47d76a96a32
ryohey <info@codingcafe.jp>
2022-05-28 07:45:41 +0900

    Add forwardRef

--- src/main/components/GLSurface/GLSurface.tsx
+++ src/main/components/GLSurface/GLSurface.tsx
@@ -1,4 +1,11 @@
-import { Component, FC, useEffect, useRef, useState } from "react"
+import {
+  Component,
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from "react"
 import { Renderable, Renderer2D } from "../../gl/Renderer2D"
 import { Shader } from "../../gl/Shader"
 import { RendererContext } from "../../hooks/useRenderer"
@@ -61,8 +68,6 @@ export abstract class GLNode
     this.context.removeObject(this)
   }
 
-  protected abstract initialize(gl: WebGLRenderingContext): void
-
   draw(): void {
     if (this.shader === null || this.buffer === null) {
       return
@@ -76,73 +81,67 @@ export abstract class GLNode
   }
 }
 
-export const isGLNode = (x: any): x is GLNode => x instanceof GLNode
+export const GLSurface = forwardRef<HTMLCanvasElement, GLSurfaceProps>(
+  ({ width, height, onCreateContext, style, children, ...props }, ref) => {
+    const canvasRef = useRef<HTMLCanvasElement>(null)
+    useImperativeHandle(ref, () => canvasRef.current!)
+    const [renderer, setRenderer] = useState<Renderer2D | null>(null)
 
-export const GLSurface: FC<GLSurfaceProps> = ({
-  width,
-  height,
-  onCreateContext,
-  style,
-  children,
-  ...props
-}) => {
-  const canvasRef = useRef<HTMLCanvasElement>(null)
-  const [renderer, setRenderer] = useState<Renderer2D | null>(null)
+    useEffect(() => {
+      const canvas = canvasRef.current
+      if (canvas === null) {
+        throw new Error("canvas is not mounted")
+      }
+      // GL コンテキストを初期化する
+      // Initialize GL context
+      const gl = canvas.getContext("webgl", {
+        alpha: true,
+        antialias: false,
+        depth: false,
+        desynchronized: true,
+        powerPreference: "high-performance",
+        premultipliedAlpha: true,
+        preserveDrawingBuffer: false,
+      })
 
-  useEffect(() => {
-    const canvas = canvasRef.current
-    if (canvas === null) {
-      throw new Error("canvas is not mounted")
-    }
-    // GL コンテキストを初期化する
-    // Initialize GL context
-    const gl = canvas.getContext("webgl", {
-      alpha: true,
-      antialias: false,
-      depth: false,
-      desynchronized: true,
-      powerPreference: "high-performance",
-      premultipliedAlpha: true,
-      preserveDrawingBuffer: false,
-    })
+      // WebGL が使用可能で動作している場合にのみ続行します
+      // Continue only if WebGL is enabled
+      if (gl === null) {
+        alert("WebGL can't be initialized. May be browser doesn't support")
+        return
+      }
 
-    // WebGL が使用可能で動作している場合にのみ続行します
-    // Continue only if WebGL is enabled
-    if (gl === null) {
-      alert("WebGL can't be initialized. May be browser doesn't support")
-      return
-    }
+      const renderer = new Renderer2D(gl)
+      setRenderer(renderer)
 
-    const renderer = new Renderer2D(gl)
-    setRenderer(renderer)
+      onCreateContext(gl)
 
-    onCreateContext(gl)
+      return () => {
+        gl?.getExtension("WEBGL_lose_context")?.loseContext()
+      }
+    }, [])
 
-    return () => {
-      gl?.getExtension("WEBGL_lose_context")?.loseContext()
-    }
-  }, [])
+    const canvasScale = window.devicePixelRatio
 
-  const canvasScale = window.devicePixelRatio
-
-  return (
-    <>
-      <canvas
-        {...props}
-        ref={canvasRef}
-        width={width * canvasScale}
-        height={height * canvasScale}
-        style={{
-          ...style,
-          width: width,
-          height: height,
-        }}
-      />
-      {renderer && (
-        <RendererContext.Provider value={renderer}>
-          {children}
-        </RendererContext.Provider>
-      )}
-    </>
-  )
-}
+    return (
+      <>
+        <canvas
+          {...props}
+          ref={canvasRef}
+          width={width * canvasScale}
+          height={height * canvasScale}
+          style={{
+            ...style,
+            width: width,
+            height: height,
+          }}
+        />
+        {renderer && (
+          <RendererContext.Provider value={renderer}>
+            {children}
+          </RendererContext.Provider>
+        )}
+      </>
+    )
+  }
+)

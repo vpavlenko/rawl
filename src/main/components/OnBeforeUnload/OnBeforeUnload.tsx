f865fcd065f98939125344e6a6868af08207cfd2
ryohey <info@codingcafe.jp>
2022-07-12 09:19:59 +0900

    Do not show confirm dialog when song is saved

--- src/main/components/OnBeforeUnload/OnBeforeUnload.tsx
+++ src/main/components/OnBeforeUnload/OnBeforeUnload.tsx
@@ -1,13 +1,19 @@
+import { observer } from "mobx-react-lite"
 import { useEffect } from "react"
 import { localized } from "../../../common/localize/localizedString"
+import { useStores } from "../../hooks/useStores"
 
-export const OnBeforeUnload = () => {
+export const OnBeforeUnload = observer(() => {
+  const rootStore = useStores()
+
   useEffect(() => {
     const listener = (e: BeforeUnloadEvent) => {
-      e.returnValue = localized(
-        "confirm-close",
-        "Your edits have not been saved. Be sure to download it before exiting. Do you really want to close it?"
-      )
+      if (!rootStore.song.isSaved) {
+        e.returnValue = localized(
+          "confirm-close",
+          "Your edits have not been saved. Be sure to download it before exiting. Do you really want to close it?"
+        )
+      }
     }
     window.addEventListener("beforeunload", listener)
 
@@ -16,4 +22,4 @@ export const OnBeforeUnload = () => {
     }
   }, [])
   return <></>
-}
+})

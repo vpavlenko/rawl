03a9f1e99068273acaeee4beac05b62ae1ad86f8
ryohey <info@codingcafe.jp>
2022-07-20 10:35:23 +0900

    Add test

--- src/common/midi/midiConversion.test.ts
+++ src/common/midi/midiConversion.test.ts
@@ -1,8 +1,11 @@
+import * as fs from "fs"
+import { AnyEvent } from "midifile-ts"
+import * as path from "path"
 import { serialize } from "serializr"
 import { emptySong } from "../song/SongFactory"
 import { NoteEvent } from "../track"
 import Track from "../track/Track"
-import { songFromMidi, songToMidi } from "./midiConversion"
+import { songFromMidi, songToMidi, songToMidiEvents } from "./midiConversion"
 
 // id for each event will not be serialized in midi file
 // we change ids sorted by order in events array
@@ -28,5 +31,42 @@ describe("SongFile", () => {
     const song2 = songFromMidi(bytes)
     song2.filepath = song.filepath // filepath will not be serialized
     expect(serialize(song2)).toStrictEqual(serialize(song))
+  })
+  describe("songToMidiEvents", () => {
+    const expectEveryTrackHaveEndOfTrackEvent = (tracks: AnyEvent[][]) => {
+      for (const track of tracks) {
+        expect(
+          track.findIndex(
+            (e) => e.type === "meta" && e.subtype === "endOfTrack"
+          )
+        ).toBe(track.length - 1)
+      }
+    }
+
+    const openFile = (fileName: string): AnyEvent[][] => {
+      const song = songFromMidi(
+        fs.readFileSync(path.join(__dirname, "../../../testdata/", fileName))
+          .buffer
+      )
+      return songToMidiEvents(song)
+    }
+
+    describe("format 1", () => {
+      const rawTracks = openFile("tracks.mid")
+
+      it("every tracks have endOfTrack event", () => {
+        expect(rawTracks.length).toBe(18)
+        expectEveryTrackHaveEndOfTrackEvent(rawTracks)
+      })
+    })
+
+    describe("format 0", () => {
+      const rawTracks = openFile("format0.mid")
+
+      it("every tracks have endOfTrack event", () => {
+        expect(rawTracks.length).toBe(17)
+        expectEveryTrackHaveEndOfTrackEvent(rawTracks)
+      })
+    })
   })
 })

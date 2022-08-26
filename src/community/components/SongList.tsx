f1ba7072a6d489389b283c5c732a8bc96394ee42
ryohey <info@codingcafe.jp>
2022-08-26 15:50:37 +0900

    Fetch songs

--- src/community/components/SongList.tsx
+++ src/community/components/SongList.tsx
@@ -1,25 +1,39 @@
-import { FC } from "react"
-import mock from "./MOCK_DATA.json"
+import { observer } from "mobx-react-lite"
+import { FC, useEffect } from "react"
+import { useStores } from "../hooks/useStores"
 import { SongListItem } from "./SongListItem"
 
-export const SongList: FC = () => {
-  const songs = mock.map((d) => ({
+export const SongList: FC = observer(() => {
+  const { authStore, communitySongStore } = useStores()
+
+  useEffect(() => {
+    ;(async () => {
+      if (authStore.user) {
+        // TODO: Remove checking user is logged-in
+        await communitySongStore.load()
+      }
+    })()
+  }, [])
+
+  const { songs } = communitySongStore
+
+  const items = songs.map((d) => ({
     song: {
       id: d.id,
-      name: d.song_name,
-      updatedAt: new Date(d.created_at),
+      name: d.data().name,
+      updatedAt: new Date(d.data().updatedAt.toDate()),
     },
     user: {
-      name: d.name,
-      photoURL: d.avatar,
+      name: d.data().userId,
+      photoURL: "",
     },
   }))
 
   return (
     <>
-      {songs.map((s) => (
+      {items.map((s) => (
         <SongListItem song={s.song} user={s.user} />
       ))}
     </>
   )
-}
+})

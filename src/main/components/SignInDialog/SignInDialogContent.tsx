21df9d6e4af73e4bed53dac17ced996b37f22336
ryohey <info@codingcafe.jp>
2022-08-26 15:49:56 +0900

    Add SignInDialogContent

---
+++ src/main/components/SignInDialog/SignInDialogContent.tsx
@@ -0,0 +1,92 @@
+import {
+  Alert,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from "@mui/material"
+import { FC } from "react"
+import { localized } from "../../../common/localize/localizedString"
+import { StyledFirebaseAuth } from "../FirebaseAuth/StyledFirebaseAuth"
+
+import styled from "@emotion/styled"
+import "firebase/auth"
+import { GithubAuthProvider, GoogleAuthProvider } from "firebase/auth"
+import { auth } from "../../firebase/firebase"
+
+const BetaLabel = styled.span`
+  border: 1px solid currentColor;
+  font-size: 0.8rem;
+  padding: 0.1rem 0.4rem;
+  margin-left: 1em;
+  color: ${({ theme }) => theme.secondaryTextColor};
+`
+
+const Description = styled.div`
+  margin: 1rem 0 2rem 0;
+  line-height: 1.5;
+`
+
+export interface SignInDialogContentProps {
+  open: boolean
+  onClose: () => void
+  onSuccess: () => void
+  onFailure: (error: firebaseui.auth.AuthUIError) => void
+}
+
+export const SignInDialogContent: FC<SignInDialogContentProps> = ({
+  open,
+  onClose,
+  onSuccess,
+  onFailure,
+}) => {
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+      keepMounted={false}
+      fullWidth
+      maxWidth="xs"
+    >
+      <DialogTitle>
+        {localized("sign-in", "Sign in")}
+        <BetaLabel>Beta</BetaLabel>
+      </DialogTitle>
+      <DialogContent>
+        <Alert severity="info">
+          {localized(
+            "cloud-beta-warning",
+            "Since the cloud function is in beta during development, please download and save your important songs frequently."
+          )}
+        </Alert>
+        <Description>
+          {localized(
+            "cloud-description",
+            "By signing in, you can save your music to the cloud and resume composing from anywhere at any time."
+          )}
+        </Description>
+        <StyledFirebaseAuth
+          uiConfig={{
+            signInOptions: [
+              GoogleAuthProvider.PROVIDER_ID,
+              GithubAuthProvider.PROVIDER_ID,
+            ],
+            callbacks: {
+              signInSuccessWithAuthResult() {
+                onSuccess()
+                return false
+              },
+              signInFailure: onFailure,
+            },
+            signInFlow: "popup",
+          }}
+          firebaseAuth={auth}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>{localized("close", "Close")}</Button>
+      </DialogActions>
+    </Dialog>
+  )
+}

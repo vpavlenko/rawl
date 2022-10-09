871bf65b479af65e915ca47a2faa2c942a04600b
ryohey <info@codingcafe.jp>
2022-10-09 09:36:10 +0900

    Move sentry setup to index.tsx

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,16 @@
+import * as Sentry from "@sentry/react"
+import { Integrations } from "@sentry/tracing"
 import { configure } from "mobx"
 import { createRoot } from "react-dom/client"
 import { App } from "./components/App/App"
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  release: process.env.VERCEL_GIT_COMMIT_SHA,
+  environment: process.env.VERCEL_ENV,
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+})
 
 configure({
   enforceActions: "never",

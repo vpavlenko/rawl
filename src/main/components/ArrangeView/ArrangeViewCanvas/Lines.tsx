859c4d8fcd6ff7c341c1453f8b2af214fb3b60b4
ryohey <info@codingcafe.jp>
2023-03-12 13:59:21 +0900

    Fix lines

--- src/main/components/ArrangeView/ArrangeViewCanvas/Lines.tsx
+++ src/main/components/ArrangeView/ArrangeViewCanvas/Lines.tsx
@@ -24,7 +24,7 @@ export const Lines: FC<{ width: number; zIndex: number }> = observer(
 
     const rects = useMemo(
       () => tracks.map((_, i) => trackHeight * (i + 1) - 1).map(hline),
-      [tracks, width]
+      [tracks, width, trackHeight]
     )
 
     const color = colorToVec4(Color(theme.dividerColor))

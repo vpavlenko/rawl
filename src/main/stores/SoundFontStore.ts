f91016922fc8a48610130f615cb895382efc3e72
ryohey <info@codingcafe.jp>
2023-09-16 17:24:08 +0900

    Update SoundFontStore

--- src/main/stores/SoundFontStore.ts
+++ src/main/stores/SoundFontStore.ts
@@ -21,7 +21,7 @@ const defaultSoundFonts: SoundFontItem[] = [
   {
     id: -999, // Use negative number to avoid conflict with user saved soundfonts
     type: "remote",
-    name: "A320U",
+    name: "A320U.sf2 (Signal Factory Sound)",
     url: "https://cdn.jsdelivr.net/gh/ryohey/signal@4569a31/public/A320U.sf2",
   },
 ]
@@ -29,28 +29,30 @@ const defaultSoundFonts: SoundFontItem[] = [
 export class SoundFontStore {
   private readonly storage: IndexedDBStorage<SoundFontItem>
   files: Metadata[] = []
-  lastSelectedSoundFontId: number | null = null
+  selectedSoundFontId: number | null = null
 
   constructor(private readonly synth: SoundFontSynth) {
     makeObservable(this, {
       files: observable,
-      lastSelectedSoundFontId: observable,
-    })
-
-    makePersistable(this, {
-      name: "SoundFontStore",
-      properties: ["lastSelectedSoundFontId"],
-      storage: window.localStorage,
+      selectedSoundFontId: observable,
     })
 
     this.storage = new IndexedDBStorage("soundfonts", 1)
-    this.init()
   }
 
-  private async init() {
+  async init() {
+    await makePersistable(this, {
+      name: "SoundFontStore",
+      properties: ["selectedSoundFontId"],
+      storage: window.localStorage,
+    })
+
     await this.storage.init()
     const savedFiles = await this.storage.list()
     this.files = [...defaultSoundFonts, ...savedFiles]
+
+    // load last selected soundfont on startup
+    await this.load(this.selectedSoundFontId ?? defaultSoundFonts[0].id)
   }
 
   private async getSoundFont(id: number): Promise<SoundFontItem | null> {
@@ -61,9 +63,7 @@ export class SoundFontStore {
     return await this.storage.load(id)
   }
 
-  async loadLastSelectedSoundFont() {
-    await this.load(this.lastSelectedSoundFontId ?? defaultSoundFonts[0].id)
-  }
+  async loadSelectedSoundFont() {}
 
   async load(id: number) {
     const soundfont = await this.getSoundFont(id)
@@ -71,6 +71,8 @@ export class SoundFontStore {
     if (soundfont === null) {
       throw new Error("SoundFont not found")
     }
+
+    this.selectedSoundFontId = id
 
     switch (soundfont.type) {
       case "local":

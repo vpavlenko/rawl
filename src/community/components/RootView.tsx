7fded20f714208c9f7936b03438f622271c92511
ryohey <info@codingcafe.jp>
2024-01-14 12:57:13 +0900

    Change file name

--- src/community/components/RootView.tsx
+++ src/community/components/RootView.tsx
@@ -1,8 +1,8 @@
 import { observer } from "mobx-react-lite"
 import { FC } from "react"
 import { Route, useLocation } from "wouter"
+import { EditProfilePage } from "../pages/EditProfilePage"
 import { HomePage } from "../pages/HomePage"
-import { EditProfilePage } from "../pages/ProfilePage"
 import { SignInDialog } from "./SignInDialog/SignInDialog"
 
 const Routes: FC = observer(() => {

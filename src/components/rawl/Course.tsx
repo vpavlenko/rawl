e6bccdeb7eb09f93b27e397177e7b0495cf37889
Vitaly Pavlenko <cxielamiko@gmail.com>
2024-01-21 18:51:35 +0400

    Fix first two lessons

--- src/components/rawl/Course.tsx
+++ src/components/rawl/Course.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import styled from "styled-components";
 import { NoteSnippet } from "./Axes";
+import { PianoLegend } from "./PianoLegend";
 
 const Row = styled.div`
   display: flex;
   flex-direction: row;
-  align-items: center;
+  align-items: start;
   gap: 40px;
 `;
 
@@ -87,24 +88,30 @@ const Course = ({ sequencer }) => {
           <NoteSnippet notes="0,1,C3-E3-G3 G3-B3-D4" sequencer={sequencer} />
         </div>
         <div>
-          These chords can be arranged using multiple octaves:
+          These chords can be arranged by doubling some notes in other octaves:
           <NoteSnippet
             notes="0,1,C2-G2-C3-E3-G3-C4 G1-G2-B2-D3-G3-B3 C2-G2-C3-E3-G3-C4 G1-G2-B2-D3-G3-D4 C2-G2-C3-E3-G3-E4"
             sequencer={sequencer}
           />
         </div>
         <div>
-          Find these chords in two songs below. Find out what notes do
-          non-melodic instruments play, relative to the chords.
+          Find these chords in two songs below. What does every instrument play,
+          structurally:
+          <ul>
+            <li>a melody?</li>
+            <li>a chord?</li>
+            <li>a lowest note of a chord?</li>
+            <li>any ornamentations to them?</li>
+          </ul>
         </div>
-        <ul>
+        <ol>
           <li>
             <S artist="MIDI/Chris Andrews" song="Pretty Belinda.1.mid" />
           </li>
           <li>
             <S artist="MIDI/Wet Willie" song="Keep on Smiling.mid" />
           </li>
-        </ul>
+        </ol>
         <h3>I-IV-V</h3>
         <div>
           Let's grow our vocabulary and add a IV chord to it. I-IV-V-I:
@@ -161,7 +168,7 @@ const Course = ({ sequencer }) => {
         </div>
         <div>
           <NoteSnippet
-            notes="0,1,C2-G2-C3-E3-G3-C4 G1-G2-B2-D3-G3-D4 A1-A2-C3-E3-A3-E4 F1-A2-C3-F3-A3-F4 C2-G2-C3-E3-G3-G4 F1-A2-C3-F3-A3-A4 G1-G2-B2-D3-G3-B4 C2-G2-C3-E3-G3-C5"
+            notes="0,1,C2-G2-C3-E3-G3-C4 G1-G2-B2-D3-G3-D4 A1-A2-C3-E3-A3-E4 F1-A2-C3-F3-A3-F4 C2-G2-C3-E3-G3-G4 A1-A2-C3-E3-A3-A4 G1-G2-B2-D3-G3-B4 C2-G2-C3-E3-G3-C5"
             sequencer={sequencer}
           />
         </div>
@@ -172,6 +179,37 @@ const Course = ({ sequencer }) => {
           </li>
           <li>
             <S artist="MIDI/Van Morrison" song="Brown Eyed Girl.mid" />
+          </li>
+        </ul>
+        <h3>ii and iii</h3>
+        <div>
+          Last two chords, which are more rare in use in major, are ii and iii.
+        </div>
+        <div>
+          I-ii-iii-ii-I:
+          <NoteSnippet
+            notes="C3-E3-G3 D3-F3-A3 E3-G3-B3 D3-F3-A3 C3-E3-G3"
+            sequencer={sequencer}
+          />
+        </div>
+        <div>
+          <NoteSnippet
+            notes="0,1,C2-G2-C3-E3-G3-C4 D2-A2-D2-F3-A3-D4 E2-B2-E3-G3-B3-E4 F2-A2-C3-F3-A3-F4 C2-G2-C3-E3-G3-G4 A1-A2-C3-E3-A3-A4 G1-G2-B2-D3-G3-B4 C2-G2-C3-E3-G3-C5"
+            sequencer={sequencer}
+          />
+        </div>
+        <div>Find all six chords in arrangements below:</div>
+        <ul>
+          <li>
+            {" "}
+            <S artist="MIDI/Westlife" song="Fool Again.mid" exercise="tonic" />
+          </li>
+          <li>
+            <S
+              artist="MIDI/Wet Wet Wet"
+              song="Angel Eyes.mid"
+              exercise="tonic"
+            />
           </li>
         </ul>
         <h2>Find a major tonic</h2>
@@ -247,33 +285,9 @@ const Course = ({ sequencer }) => {
             />
           </li>
           <li>
-            <S artist="MIDI/Westlife" song="Fool Again.mid" exercise="tonic" />
-          </li>
-        </ul>
-        <div>
-          Let's look at three chords - ii, iii and vi:
-          <NoteSnippet
-            notes="D3-F3-A3 E3-G3-B3 A3-C4-E4"
-            sequencer={sequencer}
-          />
-        </div>
-        <div>
-          These chords are so called <i>minor chords</i>. That is because of
-          intervals between the notes. Compare their sound with the sound of I,
-          IV and V above.
-        </div>
-        <ul>
-          <li>
             <S
               artist="MIDI/Typically Tropical"
               song="Barbados.mid"
-              exercise="tonic"
-            />
-          </li>
-          <li>
-            <S
-              artist="MIDI/Wet Wet Wet"
-              song="Angel Eyes.mid"
               exercise="tonic"
             />
           </li>
@@ -1113,6 +1127,18 @@ const Course = ({ sequencer }) => {
         </ul>
       </div>
       <div></div>
+      <div
+        key="piano-legend"
+        style={{
+          position: "absolute",
+          transform: "scale(5)",
+          top: 200,
+          right: 150,
+          zIndex: 10000000,
+        }}
+      >
+        <PianoLegend />
+      </div>
     </div>
   );
 };

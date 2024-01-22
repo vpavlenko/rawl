dc7a8cd40306e7a829431e6f291a36d2b1bdb5eb
Vitaly Pavlenko <cxielamiko@gmail.com>
2024-01-22 14:50:56 +0400

    Add contacts

--- src/components/rawl/course/About.tsx
+++ src/components/rawl/course/About.tsx
@@ -3,6 +3,13 @@ import * as React from "react";
 const About = ({ sequencer }) => {
   return (
     <>
+      <h3>Work in progress</h3>
+      <div>
+        This interactive book is a work in progress. Most chapters are stubs
+        with playable examples. I do free Zoom lessons on this platform to make
+        it better. If you're interested, reach out:{" "}
+        <a href="mailto:cxielamiko@gmail.com">cxielamiko@gmail.com</a>
+      </div>
       <h3>Tech details</h3>
       <div>
         Built on top of{" "}

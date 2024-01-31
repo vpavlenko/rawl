1bc0ed260942d51e69a1a47d43d3a7dfbbfb81ff
Boggy Jang <boggy@idrip.coffee>
2024-01-31 12:03:54 +0800

    add Traditional Chinese (Taiwan) translations

--- src/common/localize/localizedString.ts
+++ src/common/localize/localizedString.ts
@@ -1,6 +1,6 @@
 import localization from "./localization"
 
-export type Language = "en" | "ja" | "zh"
+export type Language = "en" | "ja" | "zh" | "tw"
 
 export function localized(
   key: string,
@@ -28,7 +28,10 @@ export function localized(
 
 function getBrowserLanguage() {
   // Use URL parameter ?lang=ja or navigator.language
-  const navigatorLanguage = navigator.language.split("-")[0]
+  // zh-CN, zh-TW を分別するために、こちらはhardcode以外今まだいい方法が浮かべません。
+  const navigatorLanguage = navigator.language.split("-")[1].toLowerCase() === 'tw'
+    ? 'tw'
+    : navigator.language.split("-")[0]
   const langParam = new URL(location.href).searchParams.get("lang")
   return langParam ?? navigatorLanguage
 }

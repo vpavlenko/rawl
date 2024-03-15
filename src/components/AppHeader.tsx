6ff95f5ef62af2e7fcc203b6af80c5c198906079
Vitaly Pavlenko <cxielamiko@gmail.com>
2024-03-15 08:01:09 +0400

    Link the rest of the MIDI library from chiptune.app into header

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -20,7 +20,72 @@ const AppHeader: React.FC = () => {
         className="AppHeader-title"
         to={{ pathname: "/browse/Classical MIDI" }}
       >
-        Classical Music
+        Classical
+      </Link>
+      {", "}
+      <Link className="AppHeader-title" to={{ pathname: "/browse/Demo MIDI" }}>
+        Demo
+      </Link>
+      {", "}
+      <Link
+        className="AppHeader-title"
+        to={{ pathname: "/browse/Contemporary" }}
+      >
+        Contemp
+      </Link>
+      {", "}
+      <Link className="AppHeader-title" to={{ pathname: "/browse/Game MIDI" }}>
+        Game
+      </Link>
+      {", "}
+      <Link className="AppHeader-title" to={{ pathname: "/browse/Jazz MIDI" }}>
+        Jazz
+      </Link>
+      {", "}
+      <Link
+        className="AppHeader-title"
+        to={{ pathname: "/browse/Piano E-Competition MIDI" }}
+      >
+        Piano
+      </Link>
+      {", "}
+      <Link
+        className="AppHeader-title"
+        to={{ pathname: "/browse/Roland SMF MIDI Disks" }}
+      >
+        Roland
+      </Link>
+      {", "}
+      <Link
+        className="AppHeader-title"
+        to={{ pathname: "/browse/Sound Canvas MIDI Collection" }}
+      >
+        SC
+      </Link>
+      {", "}
+      <Link
+        className="AppHeader-title"
+        to={{ pathname: "/browse/Tune 1000 SMF MIDI Disks" }}
+      >
+        Tune
+      </Link>
+      {", "}
+      <Link
+        className="AppHeader-title"
+        to={{ pathname: "/browse/vgmusic.com MIDI" }}
+      >
+        vg
+      </Link>
+      {", "}
+      <Link
+        className="AppHeader-title"
+        to={{ pathname: "/browse/Nintendo 64 (SoundFont MIDI)" }}
+      >
+        N64
+      </Link>
+      {", "}
+      <Link className="AppHeader-title" to={{ pathname: "/browse/OnlyMIDIs" }}>
+        Only
       </Link>
       {" • "}
       <a href="https://github.com/vpavlenko/rawl" target="_blank">

7d3a867c51a39bc1ab7c72322d153f8d901dae12
Vitaly Pavlenko <cxielamiko@gmail.com>
2024-04-29 12:32:33 +0100

    Add a url slug for firestore midis, build their index in a separate document

--- src/components/rawl/midiStorage.tsx
+++ src/components/rawl/midiStorage.tsx
@@ -1,9 +1,21 @@
 import {
   Bytes,
   addDoc,
+  arrayUnion,
   collection,
+  doc,
   getFirestore,
+  updateDoc,
 } from "firebase/firestore/lite";
+
+function formatForURL(title: string): string {
+  let processedTitle = title.toLowerCase();
+  processedTitle = processedTitle.replace(/[^a-z0-9\s-]/g, "_");
+  processedTitle = processedTitle.replace(/[\s-]+/g, "_");
+  processedTitle = processedTitle.replace(/_+/g, "_");
+  processedTitle = processedTitle.replace(/^_+|_+$/g, "");
+  return processedTitle;
+}
 
 export const saveMidi = async (link: string) => {
   try {
@@ -12,6 +24,7 @@ export const saveMidi = async (link: string) => {
     const link = atob(params.get("link") || "");
     const url = atob(params.get("pageUrl") || "");
     const title = atob(params.get("title") || "");
+    const slug = formatForURL(title);
 
     const response = await fetch(`https://corsproxy.io/?${link}`, {
       method: "GET",
@@ -31,8 +44,14 @@ export const saveMidi = async (link: string) => {
       blob: firestoreBlob,
       url,
       title,
+      slug,
     });
     console.log("Document written with ID:", docRef.id);
+
+    const indexRef = doc(firestore, "indexes", "midis");
+    await updateDoc(indexRef, {
+      midis: arrayUnion({ title, slug, id: docRef.id }),
+    });
   } catch (error) {
     alert(`Fetching binary file failed: ${JSON.stringify(error)}`);
   }

be1615f1fec1bc10d2614daa10af119048edcbeb
Vitaly Pavlenko <cxielamiko@gmail.com>
2024-07-21 18:44:32 +0400

    Update corpus

--- src/components/rawl/corpora/Corpus.tsx
+++ src/components/rawl/corpora/Corpus.tsx
@@ -1,9 +1,23 @@
 import * as React from "react";
 
+// I skip "14 I… 48:33" since it's basically track 1 with added violin.
+// I skip "13 Dream A Little Dream Of Me 46:25" since it's not original.
+// I skip "07 When The Love Falls 23:28" since it's not original.
+// For "05 Passing By 15:17" and "09 Time Forgets… 29:52" I use versions without a cello, since
+// it's doubling a piano melody anyways.
 const corpora = [
   {
     slug: "first_love",
-    midis: ["i---yiruma"],
+    midis: [
+      "i---yiruma",
+      "may-be---yiruma",
+      "yiruma---love-me",
+      "river-flows-in-you",
+      "passing-by---yiruma",
+      "it-s-your-day---yiruma",
+      "left-my-hearts---yiruma",
+      "time-forgets---yiruma",
+    ],
   },
 ];
 
@@ -17,9 +31,11 @@ const Corpus: React.FC<{ slug: string }> = ({ slug }) => {
     <div>
       <h1>{slug}</h1>
       {corpus.midis.map((midiSlug) => (
-        <a href={`/f/${midiSlug}`} target="_blank">
-          {midiSlug}
-        </a>
+        <div>
+          <a href={`/f/${midiSlug}`} target="_blank">
+            {midiSlug}
+          </a>
+        </div>
       ))}
     </div>
   );

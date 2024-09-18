048670b51a26409080ce2f2d64c41584193f9dd0
Vitaly Pavlenko <cxielamiko@gmail.com>
2024-09-18 13:12:20 +0400

    add more h-mappings

--- src/keySlugMapping.ts
+++ src/keySlugMapping.ts
@@ -1,6 +1,19 @@
 const keySlugMapping: Record<string, string> = {
   "match-1": "Maple_Leaf_Rag_Scott_Joplin",
   "match-2": "s.-xarhakos---stoy-thoma---greek-music-055",
+  "match-3": "may-be---yiruma",
+  "match-4": "Sweden_Minecraft",
+  "match-5": "arabesque-l.-66-no.-1-in-e-major",
+  "match-6": "s.-xarhakos---mpoyrnovalia---greek-music-079",
+  "match-7": "claude-debussy---la-fille-aux-cheveux-de-lin",
+  "match-8": "gladiolus-rag---scott-joplin---1907",
+  "match-9": "it-s-your-day---yiruma",
+  "match-10": "dry-hands-minecraft",
+  "match-11": "",
+  "match-12": "",
+  "match-13": "",
+  "match-14": "",
+  "match-15": "",
 };
 
 export default keySlugMapping;

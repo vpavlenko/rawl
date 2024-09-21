b7996ca60772890e1d712718a4982d800c07beb2
Vitaly Pavlenko <cxielamiko@gmail.com>
2024-09-21 22:12:40 +0400

    remove disabled categories from AppHeader

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -7,76 +7,7 @@ const AppHeader: React.FC = () => {
       <Link className="AppHeader-title" to={{ pathname: "/" }}>
         Rawl
       </Link>
-      {" • "}
-      <Link className="AppHeader-title" to={{ pathname: "/browse/MIDI" }}>
-        Rock
-      </Link>
-      {" • "}
-      <Link className="AppHeader-title" to={{ pathname: "/browse/Demo MIDI" }}>
-        d
-      </Link>{" "}
-      <Link
-        className="AppHeader-title"
-        to={{ pathname: "/browse/Roland SMF MIDI Disks" }}
-      >
-        r
-      </Link>{" "}
-      <Link
-        className="AppHeader-title"
-        to={{ pathname: "/browse/Piano E-Competition MIDI" }}
-      >
-        e
-      </Link>{" "}
-      <Link
-        className="AppHeader-title"
-        to={{ pathname: "/browse/vgmusic.com MIDI" }}
-      >
-        v
-      </Link>{" "}
-      <Link
-        className="AppHeader-title"
-        to={{ pathname: "/browse/Nintendo 64 (SoundFont MIDI)" }}
-      >
-        n
-      </Link>{" "}
-      <Link className="AppHeader-title" to={{ pathname: "/browse/OnlyMIDIs" }}>
-        o
-      </Link>{" "}
-      <Link
-        className="AppHeader-title"
-        to={{ pathname: "/browse/Sound Canvas MIDI Collection" }}
-      >
-        s
-      </Link>{" "}
-      <Link
-        className="AppHeader-title"
-        to={{ pathname: "/browse/Tune 1000 SMF MIDI Disks" }}
-      >
-        t
-      </Link>{" "}
-      <Link
-        className="AppHeader-title"
-        to={{ pathname: "/browse/Classical MIDI" }}
-      >
-        cl
-      </Link>{" "}
-      <Link
-        className="AppHeader-title"
-        to={{ pathname: "/browse/Contemporary" }}
-      >
-        ct
-      </Link>{" "}
-      <Link className="AppHeader-title" to={{ pathname: "/browse/Game MIDI" }}>
-        g
-      </Link>{" "}
-      <Link className="AppHeader-title" to={{ pathname: "/browse/Jazz MIDI" }}>
-        j
-      </Link>{" "}
-      {" • "}
-      <Link className="AppHeader-title" to={{ pathname: "/pages/daw" }}>
-        DAW
-      </Link>{" "}
-      {" • "}
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       <a href="https://github.com/vpavlenko/rawl" target="_blank">
         <div className="octocat" />
       </a>

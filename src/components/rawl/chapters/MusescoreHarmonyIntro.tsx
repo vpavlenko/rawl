d8226882a798645b8c1858133bf441a48b2763a1
Vitaly Pavlenko <cxielamiko@gmail.com>
2024-11-01 08:19:13 +0400

    link slugs on chapters

--- src/components/rawl/chapters/MusescoreHarmonyIntro.tsx
+++ src/components/rawl/chapters/MusescoreHarmonyIntro.tsx
@@ -1,6 +1,190 @@
 import * as React from "react";
 import { corpora } from "../corpora/corpora";
 
+const TOP_100_COMPOSERS = [
+  { slug: "river-flows-in-you", composer: "Yiruma" },
+  {
+    slug: "Merry_Go_Round_of_Life_Howls_Moving_Castle_Piano_Tutorial_",
+    composer: "Joe Hisaishi",
+  },
+  { slug: "Canon_in_D", composer: "Pachelbel" },
+  { slug: "Clair_de_Lune__Debussy", composer: "Debussy" },
+  { slug: "Fr_Elise", composer: "Beethoven" },
+  { slug: "Chopin_-_Nocturne_Op_9_No_2_E_Flat_Major", composer: "Chopin" },
+  { slug: "Gymnopdie_No._1__Satie", composer: "Satie" },
+  { slug: "Undertale_-_Megalovania_Piano_ver._3", composer: "Toby Fox" },
+  { slug: "Golden_Hour__JVKE_Updated_Ver.", composer: "JVKE" },
+  {
+    slug: "Je_Te_Laisserai_Des_Mots_-_Patrick_Watson",
+    composer: "Patrick Watson",
+  },
+
+  { slug: "Hallelujah", composer: "Leonard Cohen" },
+  { slug: "Interstellar", composer: "Hans Zimmer" },
+  { slug: "Another_Love__-_Tom_Odell_Professional", composer: "Tom Odell" },
+  { slug: "Sweden_Minecraft", composer: "C418" },
+  { slug: "Pirates_of_the_Caribbean_-_Hes_a_Pirate", composer: "Klaus Badelt" },
+  { slug: "Game_of_Thrones_Easy_piano", composer: "Ramin Djawadi" },
+  { slug: "Someone_You_Loved", composer: "Lewis Capaldi" },
+  { slug: "Ed_Sheeran_Perfect", composer: "Ed Sheeran" },
+  { slug: "Liebestraum_No._3_in_A_Major", composer: "Liszt" },
+  { slug: "Believer_-_Imagine_Dragons", composer: "Imagine Dragons" },
+
+  {
+    slug: "All_Of_Me_-_John_Legend_Piano_Cover_-_ReiK",
+    composer: "John Legend",
+  },
+  { slug: "Mad_world_Piano", composer: "Roland Orzabal" },
+  {
+    slug: "mariage-d-amour---paul-de-senneville-marriage-d-amour",
+    composer: "Paul de Senneville",
+  },
+  { slug: "Someone_Like_You_easy_piano", composer: "Adele" },
+  { slug: "my-heart-will-go-on", composer: "James Horner" },
+  {
+    slug: "Jojo_s_Bizarre_Adventure_Golden_Wind_Giornos_Theme_Ver_2",
+    composer: "Yugo Kanno",
+  },
+  { slug: "Carol_of_the_Bells", composer: "Mykola Leontovych" },
+  { slug: "piano-man-piano", composer: "Billy Joel" },
+  { slug: "Fly_Me_to_the_Moon", composer: "Bart Howard" },
+  { slug: "passacaglia---handel-halvorsen", composer: "Handel" },
+
+  {
+    slug: "prelude-i-in-c-major-bwv-846---well-tempered-clavier-first-book",
+    composer: "Bach",
+  },
+  { slug: "All_I_Want_for_Christmas_is_You", composer: "Mariah Carey" },
+  {
+    slug: "Waltz_No._2_The_Second_Waltz_by_Dmitri_Shostakovich_for_Piano",
+    composer: "Shostakovich",
+  },
+  {
+    slug: "wa-mozart-marche-turque-turkish-march-fingered",
+    composer: "Mozart",
+  },
+  { slug: "Viva_La_Vida_Coldplay", composer: "Coldplay" },
+  { slug: "Gravity_Falls_Opening", composer: "Brad Breeck" },
+  { slug: "the_entertainer_scott_joplin", composer: "Scott Joplin" },
+  { slug: "Disney_Pixar_Up_Theme", composer: "Michael Giacchino" },
+  { slug: "a-thousand-years", composer: "Christina Perri" },
+  { slug: "John_Lennon_Imagine", composer: "John Lennon" },
+
+  {
+    slug: "runaway---kanye-west-ramin-djawadi-arr.-by-alex-patience",
+    composer: "Emile Haynie",
+  },
+  { slug: "Lovely_Billie_Eilish", composer: "Billie Eilish" },
+  { slug: "Omori_Duet", composer: "Pedro Silva" },
+  { slug: "Never_Gonna_Give_You_Up", composer: "Mike Stock" },
+  { slug: "despacito-piano-cover-peter-bence", composer: "Luis Fonsi" },
+  { slug: "solas---jamie-duffy", composer: "Jamie Duffy" },
+  { slug: "autumn-leaves-jazz-piano", composer: "Joseph Kosma" },
+  { slug: "still-dre---variation-composition", composer: "Andre Young" },
+  {
+    slug: "what-falling-in-love-feels-like---jake25.17-fanmade-extended-version",
+    composer: "Jake 25.17",
+  },
+  { slug: "mii-channel-piano", composer: "Kazumi Totaka" },
+
+  { slug: "sadness-and-sorrow-for-piano-solo", composer: "Toshio Masuda" },
+  { slug: "Super_Mario_Bros_Main_Theme", composer: "Koji Kondo" },
+  { slug: "Cant_Help_Falling_In_Love", composer: "Jean-Paul-Égide Martini" },
+  { slug: "g-minor-bach-original", composer: "Luo Ni" },
+  { slug: "when-i-was-your-man---bruno-mars-600e3a", composer: "Bruno Mars" },
+  {
+    slug: "gurenge--demon-slayer-kimetsu-no-yaiba-op",
+    composer: "Kayoko Kusano",
+  },
+  { slug: "Let_Her_Go_Passenger", composer: "Passenger" },
+  {
+    slug: "we-are-number-one-but-it-s-a-piano-transcript",
+    composer: "Máni Svavarsson",
+  },
+  {
+    slug: "dragonborn---skyrim-theme-song-piano-solo",
+    composer: "Jeremy Soule",
+  },
+  {
+    slug: "doki-doki-literature-club-ost---your-reality",
+    composer: "Dan Salvato",
+  },
+
+  { slug: "ylang-ylang---fkj-transcribed-by-lilroo", composer: "FKJ" },
+  {
+    slug: "attack-on-titan-theme-guren-no-yumiya",
+    composer: "Hiroyuki Sawano",
+  },
+  { slug: "Bella_Ciao", composer: "Italian folk" },
+  { slug: "minuet-bwv-anhang-114-in-g-major", composer: "Christian Petzold" },
+  { slug: "Take_on_me", composer: "a-ha" },
+  { slug: "congratulations---mac-miller", composer: "Mac Miller" },
+  { slug: "the-office-opening-theme", composer: "Jay Ferguson" },
+  { slug: "it-s-been-a-long-long-time---harry-james", composer: "Jule Styne" },
+  { slug: "Dawn_Pride_and_Prejudice", composer: "Dario Marianelli" },
+  { slug: "kimi-no-na-wa---sparkle-theishter-2016", composer: "Radwimps" },
+
+  { slug: "Yann_Tiersen_Amelie", composer: "Yann Tiersen" },
+  { slug: "sia---snowman", composer: "Sia" },
+  {
+    slug: "isabella-s-lullaby-the-promised-neverland-emotional-anime-on-piano-vol.-2",
+    composer: "Takahiro Obata",
+  },
+  {
+    slug: "theme-from-schindler-s-list---piano-solo",
+    composer: "John Williams",
+  },
+  { slug: "happy-birthday-to-you-c-major", composer: "Patty Smith Hill" },
+  { slug: "flight-of-the-bumblebee", composer: "Nikolai Rimsky-Korsakov" },
+  {
+    slug: "dance-of-the-sugar-plum-fairy",
+    composer: "Pyotr Ilyich Tchaikovsky",
+  },
+  { slug: "dont-stop-believing-piano", composer: "Journey" },
+  { slug: "sign-of-the-times---harry-styles", composer: "Harry Styles" },
+  { slug: "Requiem_for_a_Dream", composer: "Clint Mansell" },
+
+  {
+    slug: "yuri-on-ice---piano-theme-full",
+    composer: "Taro Umebayashi + Taku Matsushiba",
+  },
+  {
+    slug: "ye-niqu-keru-yoru-ni-kakeru---racing-into-the-night",
+    composer: "Ayase",
+  },
+  { slug: "africa---toto", composer: "Toto" },
+  { slug: "vivaldi---summer---piano", composer: "Vivaldi" },
+  { slug: "Love_Like_You_Steven_Universe", composer: "Steven Universe" },
+  { slug: "alan-walker---alone-piano", composer: "Alan Walker" },
+  {
+    slug: "my-lie-watashi-no-uso---your-lie-in-april",
+    composer: "Masaru Yokoyama",
+  },
+  { slug: "anastasia---once-upon-a-december", composer: "Stephen Flaherty" },
+  { slug: "Test_Drive_How_to_Train_Your_Dragon", composer: "John Powell" },
+  { slug: "Pokemon_Theme_Song", composer: "John Siegler" },
+
+  { slug: "your-song-piano", composer: "Elton John" },
+  { slug: "nothing-else-matters---metallica", composer: "Metallica" },
+  {
+    slug: "calum-scott---you-are-the-reason-piano-sheet-lyrics-lyrics-version-link-in-description",
+    composer: "Calum Scott",
+  },
+  { slug: "fairy-tail-main-theme", composer: "Yasuharu Takanashi" },
+  {
+    slug: "welcome-to-the-black-parade---my-chemical-romance",
+    composer: "My Chemical Romance",
+  },
+  { slug: "how-far-i-ll-go-~-moana-ost", composer: "Lin-Manuel Miranda" },
+  { slug: "la-vie-en-rose-solo-accordion", composer: "Louiguy" },
+  { slug: "Im_Blue_Eiffel_65", composer: "Eiffel 65" },
+  {
+    slug: "old-town-road---lil-nas-x-ft.-billy-ray-cyrus",
+    composer: "Lil Nas X",
+  },
+  { slug: "abba--the-winner-takes-it-all", composer: "ABBA" },
+];
+
 const MusescoreHarmonyIntro = () => {
   const missingSlugs =
     corpora
@@ -30,8 +214,20 @@ const MusescoreHarmonyIntro = () => {
       </p>
 
       <p>
+        Here's a complete list of 100 composers in the corpus:{" "}
+        {TOP_100_COMPOSERS.map(({ slug, composer }, i) => (
+          <>
+            <a href={`/f/${slug}`} target="_blank" rel="noreferrer">
+              {composer}
+            </a>
+            {i < TOP_100_COMPOSERS.length - 1 && ", "}
+          </>
+        ))}
+      </p>
+
+      <p>
         In most cases, I take the same arrangement that scored the most view
-        counts, but sometimes I find an arrangement that suits better to my
+        counts. Sometimes I find an arrangement that suits better to my
         narrative - maybe by having less mistakes, original key, better MIDI
         sound or so.
       </p>
@@ -40,7 +236,7 @@ const MusescoreHarmonyIntro = () => {
         I only take solo piano arrangements because I want to focus on harmony,
         not on the arrangement. For "We Are Number One" the most popular version
         is a band arrangement, and I represent this piece by its piano
-        arrangements.
+        arrangement.
       </p>
 
       <p>

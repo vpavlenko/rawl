0648143a56155becb151d32ebbc7210f9144debc
Vitaly Pavlenko <cxielamiko@gmail.com>
2024-12-02 11:57:42 +0500

    simplify drop message

--- src/components/DropMessage.tsx
+++ src/components/DropMessage.tsx
@@ -1,10 +1,4 @@
 import * as React from "react";
-const { FORMATS } = require("../config");
-
-const formatList = FORMATS.filter((f) => f !== "miniusf").map((f) => `.${f}`);
-const splitPoint = Math.floor(formatList.length / 2) - 1;
-const formatsLine1 = `Formats: ${formatList.slice(0, splitPoint).join(" ")}`;
-const formatsLine2 = formatList.slice(splitPoint).join(" ");
 
 const DropMessage: React.FC<{
   dropzoneProps: {
@@ -16,16 +10,7 @@ const DropMessage: React.FC<{
       hidden={!dropzoneProps.isDragActive}
       className="message-box drop-message"
     >
-      <div className="message-box-inner">
-        Drop files to play!
-        <br />
-        {formatsLine1}
-        <br />
-        {formatsLine2}
-        <br />
-        <br />
-        Drop a SoundFont (.sf2) to customize MIDI playback.
-      </div>
+      <div className="message-box-inner">Drop a MIDI file to play.</div>
     </div>
   </div>
 );

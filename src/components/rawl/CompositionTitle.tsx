aeb2574f32dc8969cd7f774d276e223222524b50
Vitaly Pavlenko <cxielamiko@gmail.com>
2024-12-24 19:18:14 +0100

    add a musescore icon

--- src/components/rawl/CompositionTitle.tsx
+++ src/components/rawl/CompositionTitle.tsx
@@ -16,6 +16,13 @@ import { corpora, MUSESCORE_TOP_100_SLUG } from "./corpora/corpora";
 import { CorpusLink } from "./corpora/CorpusLink";
 import { TOP_100_COMPOSERS } from "./top100Composers";
 
+const MUSESCORE_ICON = `
+<?xml version="1.0" encoding="utf-8"?>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 32">
+  <path d="M5.529 8.944h.087c.088-3.375 2.677-6.18 5.968-6.18 2.15 0 3.685.92 4.475 2.19-1.14.482-2.018 1.622-2.018 3.025 0 1.885 1.58 3.331 3.51 3.331 1.93 0 3.51-1.622 3.51-3.769 0-3.638-3.334-6.97-7.766-6.97-4.43 0-7.81 3.55-7.81 7.715 0 .175 0 .35.044.657m13.69 13.369 3.64-5.61L6.363 9.644l-.044.043 12.9 12.624Zm4.65-3.726c0 4.821-4.125 8.152-8.336 8.152-3.291 0-5.705-1.183-6.889-2.805 1.492-.877 2.37-2.367 2.37-4.251 0-2.806-2.282-5.085-5.31-5.085C2.502 14.597 0 17.183 0 20.558c0 5.48 5.177 10.87 12.329 10.87C19.48 31.429 24 25.819 24 19.464c0-.351 0-.57-.044-.877h-.087Z" fill="white"/>
+</svg>
+`;
+
 export type CompositionTitleProps = {
   slug: string;
   sourceUrl: string | null;
@@ -121,10 +128,22 @@ const CompositionTitle: React.FC<CompositionTitleProps> = ({
                 cursor: "pointer",
               }}
             >
-              <FontAwesomeIcon
-                icon={faArrowUpRightFromSquare}
-                style={{ width: "15px", marginLeft: "10px" }}
-              />
+              {sourceUrl.includes("musescore.com") ? (
+                <span
+                  style={{
+                    display: "inline-block",
+                    width: "12px",
+                    height: "12px",
+                    marginLeft: "20px",
+                  }}
+                  dangerouslySetInnerHTML={{ __html: MUSESCORE_ICON }}
+                />
+              ) : (
+                <FontAwesomeIcon
+                  icon={faArrowUpRightFromSquare}
+                  style={{ width: "15px", marginLeft: "10px" }}
+                />
+              )}
             </a>
           ) : (
             <>

e3c18e5fae2a08e7103edc0b4d7f49acaa07be9d
Vitaly Pavlenko <cxielamiko@gmail.com>
2025-01-24 09:45:19 +0100

    more handel

--- src/components/rawl/editor/Editor.tsx
+++ src/components/rawl/editor/Editor.tsx
@@ -458,10 +458,17 @@ lh
 11 copy 3-10 0 0 0 0 0 0 0
 67 1|
 68 copy 67 -4 -1 -5 -2 -4 -3 0
+67 copy 67-74 7
 74 1-
 rh
+9 ^1_7-6-7_.1-1|
+11 copy 3-10 0 0 0 0 0
 3 ^b3-^b3-2-b3-1-b3-b7-b3-vb6-^b3-v5-^b3-v4-^b3-vb3-^b3-
-5 copy 3-4 -1 -2`);
+5 copy 3-4 -1 -2
+11 ^^b3-vb3-4-b3-5-b3-b6-b3-^b7-vb3-^1-vb3-^2-vb3-^b3-vb3-
+13 copy 11-12 -1 -2
+17 copy 9-10 0
+51 copy 3-18 0`);
   const [context, setContext] = useState<CommandContext>({
     currentKey: { tonic: 0, mode: "major" }, // Default to C major
     currentTrack: 1, // Default to right hand (track 2)

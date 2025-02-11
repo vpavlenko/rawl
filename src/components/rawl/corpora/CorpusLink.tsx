ad1e1752ef44cf22c4b4799946d54d960c91f31a
Vitaly Pavlenko <cxielamiko@gmail.com>
2025-02-11 09:47:34 +0100

    dont capitalize "de"

--- src/components/rawl/corpora/CorpusLink.tsx
+++ src/components/rawl/corpora/CorpusLink.tsx
@@ -19,7 +19,12 @@ const badgeStyle = {
 const capitalizeWords = (text: string): string => {
   return text
     .split(" ")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .map((word, index) => {
+      if (word.toLowerCase() === "de" && index !== 0) {
+        return "de";
+      }
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
     .join(" ");
 };
 

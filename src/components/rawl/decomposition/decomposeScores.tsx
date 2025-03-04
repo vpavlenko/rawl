05eb243b9ffebfbaa1cf4a8c381879790fc842af
Vitaly Pavlenko <cxielamiko@gmail.com>
2025-03-05 00:28:30 +0400

    start typing /d/idea-15

--- src/components/rawl/decomposition/decomposeScores.tsx
+++ src/components/rawl/decomposition/decomposeScores.tsx
@@ -19,23 +19,18 @@ export const decomposeScores: { [key: string]: DecomposedScore } = {
     steps: [
       {
         score:
-          "C major\n4/4\nbpm 120\n\nlh\n1 i 1,. qet,. qet,\n\nrh\n1 i x-q-w,e-r,t,",
-        explanation: "",
+          "Db minor\n\nlh\n1   i 1=2=3=4=5=6=7=8=\n1b3 c 1-1b3 7 14 21\n3   i 1=q=a=x=\n3b2 c 3-3b2 1 2 3 4 5 6 7",
+        explanation: "Let's use the Db minor scale.",
+      },
+      {
+        score: "Db minor\n6/4\n\nrh\n1 i 1,3,5,x,135_\n2 c 1 1 2 3 4 5 6 7",
+        explanation: "Let's build triads on all 7 notes of this scale.",
       },
       {
         score:
-          "C major\n4/4\nbpm 120\n\nlh\n1 i 1,. qet,. qet,  \n2 c 1 3 4 0\n\nrh\n1 i x-q-w,e-r,t,\n2 c 1 1 2",
-        explanation: "Pick a bass line 4 - 6 - 1 - 7 in 6/4.",
-      },
-      {
-        score:
-          "C major\n4/4\nbpm 120\n\nlh\n1 i 1,. qet,. qet,  \n2 c 1 3 4 0\n\nrh\n1 i x-q-w,e-r,t,\n2 c 1 1 2\n4 i x,a_.\n\n5 ac 1-4",
-        explanation: "",
-      },
-      {
-        score:
-          "C major\n4/4\nbpm 120\n\nlh\n1 i 1,. qet,. qet,  \n2 c 1 3 4 0\n\nrh\n1 i x-q-w,e-r,t,\n2 c 1 1 2\n4 i x,a_.\n\n5 ac 1-4",
-        explanation: "",
+          "Db minor\n6/4\nbpm 180\nphrases 5-1\nsections 2 4\n\nlh\n1 i qet_\n1b3 c 1-1b3 1 2 3 4 5 6 7\n4 i 1,qe,qe, 5,qe,qe,\n5 c 4 1 2 3 4 5 6 7\n\n12 c 7\n13 c 9\n14 c 11\n15 c 10\n\n16 c 12-15",
+        explanation:
+          "Let's play them together.\n\nThen let's play all of them in a pattern.\n\nAnd then let's only pick four chords we need: 4 - 6 - 1 - 7. Let's make it a loop and repeat it twice.",
       },
     ],
   },

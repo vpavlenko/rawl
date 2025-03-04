07912d9f177305611532ca92888c4384823930fa
Vitaly Pavlenko <cxielamiko@gmail.com>
2025-03-04 21:11:11 +0400

    clean up decomposition

--- src/components/rawl/decomposition/Decomposition.tsx
+++ src/components/rawl/decomposition/Decomposition.tsx
@@ -1,70 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { RouteComponentProps, withRouter } from "react-router-dom";
-import styled from "styled-components";
 import { VoiceMask } from "../../App";
 import { Analysis } from "../analysis";
 import Editor from "../editor/Editor";
 import DecompositionGuide from "./DecompositionGuide";
 import { decomposeScores } from "./decomposeScores";
-
-const StepNavigationContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 16px;
-  background-color: #f5f5f5;
-  border-radius: 4px;
-  margin-bottom: 16px;
-`;
-
-const NavigationControls = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 16px;
-`;
-
-const NavigationButton = styled.button`
-  background-color: #4a90e2;
-  color: white;
-  border: none;
-  border-radius: 4px;
-  padding: 8px 16px;
-  cursor: pointer;
-  font-weight: bold;
-
-  &:disabled {
-    background-color: #cccccc;
-    cursor: not-allowed;
-  }
-
-  &:hover:not(:disabled) {
-    background-color: #3a80d2;
-  }
-`;
-
-const StepInfo = styled.div`
-  text-align: center;
-  font-size: 16px;
-  font-weight: bold;
-`;
-
-const ExplanationContainer = styled.div`
-  background-color: #fff;
-  border: 1px solid #e0e0e0;
-  border-radius: 4px;
-  padding: 16px;
-`;
-
-const ExplanationTitle = styled.h3`
-  margin-top: 0;
-  margin-bottom: 8px;
-  color: #333;
-`;
-
-const ExplanationContent = styled.p`
-  margin: 0;
-  line-height: 1.5;
-`;
 
 interface DecompositionProps extends RouteComponentProps {
   slug: string;
